d0b6dd8589fdfe80bc906fd926cb9e621538246a
cheponin <nina.chepovska@gmail.com>
2015-11-07 10:31:32 -0800

    Added test package with BidderTest and BidTest JUnit classes.

---
+++ AuctionCentral/test/BidTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BidTest {
+
+	@Test
+	public void testBid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBidAmount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBidAmount() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ AuctionCentral/test/BidderTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BidderTest {
+
+	@Test
+	public void testBidder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testViewBids() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddBid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveBid() {
+		fail("Not yet implemented");
+	}
+
+}
