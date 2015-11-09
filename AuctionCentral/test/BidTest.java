1c8da19ed1b24d0a4139e1b71316a44793ffdab0
cheponin <nina.chepovska@gmail.com>
2015-11-09 09:33:12 -0800

    Implemented the BidTest class, all tests pass except for one.

--- AuctionCentral/test/BidTest.java
+++ AuctionCentral/test/BidTest.java
@@ -2,28 +2,51 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.awt.Image;
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import model.Bid;
+import model.Item;
+
+/**
+ * JUnit tests for the Bid class.
+ * 
+ * @author Nina Chepovska
+ * @version
+ */
 public class BidTest {
+	
+	Item myItem;
+	Bid myBid;
+	
+	@Before
+	public void setup() {
+		myItem = new Item("Golf Clubs", 6, new ArrayList<Image>(), "Six golf clubs");
+		myBid = new Bid(myItem, 225.0);
+	}
 
 	@Test
 	public void testBid() {
-		fail("Not yet implemented");
+		Bid bid = new Bid(new Item("Golf Clubs", 6, new ArrayList<Image>(), "Six golf clubs"), 225.0);
+		assertEquals(myBid, bid);
 	}
 
 	@Test
 	public void testGetItem() {
-		fail("Not yet implemented");
+		assertEquals(myItem, myBid.getItem());
 	}
 
 	@Test
 	public void testGetBidAmount() {
-		fail("Not yet implemented");
+		assertEquals(225.0, myBid.getBidAmount(), 0.0001);
 	}
 
 	@Test
 	public void testSetBidAmount() {
-		fail("Not yet implemented");
+		myBid.setBidAmount(350.0);
+		assertEquals(350.0, myBid.getBidAmount(), 0.0001);
 	}
-
 }
