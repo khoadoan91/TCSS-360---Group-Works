86c1b0592ccdf91f33587e7f9cd8aad147462802
nfadili <nfadili@uw.edu>
2015-11-09 10:58:49 -0800

    Added userType to Bidder. Will need to sync with Nina's changes later

--- AuctionCentral/model/Bidder.java
+++ AuctionCentral/model/Bidder.java
@@ -1,9 +1,14 @@
 package model;
 
 public class Bidder extends User {
+	
+	final private String userType = "Bidder";
 
 	public Bidder(String username) {
 		super(username);
 	}
 	
+	public String getUserType() {
+		return userType;
+	}
 }
