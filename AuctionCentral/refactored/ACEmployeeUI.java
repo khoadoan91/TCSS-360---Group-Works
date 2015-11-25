f75806ef11db453df0ee8397b228cec0d6018167
nfadili <nfadili@uw.edu>
2015-11-25 13:26:51 -0800

    Fixed ACEmployeeUI menu and added month switching functionality.

--- AuctionCentral/refactored/ACEmployeeUI.java
+++ AuctionCentral/refactored/ACEmployeeUI.java
@@ -3,6 +3,7 @@
  */
 package refactored;
 
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -19,7 +20,7 @@ public class ACEmployeeUI implements UserUI {
 	 * Accesses calendar singleton and prints upcoming auctions.
 	 * 
 	 */
-	public void viewUpcomingAuction(Scanner scanner, DisplayCalendar cal) {
+	private void viewUpcomingAuction(Scanner scanner, DisplayCalendar cal) {
 		System.out.println("Please pick one auction for detail. ");
 		List<Auction> upcomingAuc = cal.getUpcomingAuctions();
 		for (int i = 0; i < upcomingAuc.size(); i++) {
@@ -35,20 +36,37 @@ public class ACEmployeeUI implements UserUI {
 	 * Runs main menu and prompts for an ACEmployee.
 	 */
 	@Override
-	public void promptMainMenu(Scanner scanner, DisplayCalendar cal) {
+	public void promptMainMenu(Scanner scanner, DisplayCalendar theCalendar) {
 		boolean isQuit = false;
 		do {
 			// TODO view next month, view past auction
-			System.out.println("Please choose an option below.");
-			System.out.println("1.  View a calendar");
+			System.out.println("Please choose an option below or type any other number to exit.");
+			System.out.println("1.  View the calendar");
 			System.out.println("2.  View upcoming auction");
-			System.out.print("------Done!! want to exit? type any number ");
+			//System.out.print("#.  Exit");
 			switch (scanner.nextInt()) {
-				case 1: System.out.println(cal); break;
-				case 2: viewUpcomingAuction(scanner, cal); break;
+				case 1: viewCalendar(scanner, theCalendar); break;
+				case 2: viewUpcomingAuction(scanner, theCalendar); break;
 				default: isQuit = true; break;
 			}
 		} while (!isQuit);	
+	}
+	
+	private void viewCalendar(Scanner scanner, DisplayCalendar theCalendar) {
+		Calendar timeRequested = Calendar.getInstance();
+		boolean isQuit = false;
+		do {
+			System.out.println(theCalendar.displayCalendar(timeRequested));
+			System.out.println("1.  Previous month\n" +
+							   "2.  Next month\n" +
+							   "3.  Back to main menu");
+			switch (scanner.nextInt()) {
+			case 1: timeRequested.set(Calendar.MONTH, timeRequested.get(Calendar.MONTH) - 1); break;
+			case 2: timeRequested.set(Calendar.MONTH, timeRequested.get(Calendar.MONTH) + 1); break;
+			case 3: isQuit = true; break;
+			default: System.out.println("Invalid input"); break;
+			}
+		} while(!isQuit);	
 	}
 
 }
