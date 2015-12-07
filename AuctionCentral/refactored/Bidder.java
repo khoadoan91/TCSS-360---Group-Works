2efd67932b2228ac867aed153f66db914c39a784
cheponin <nina.chepovska@gmail.com>
2015-12-07 09:40:55 -0800

    Added documentation to Bidder.

--- AuctionCentral/refactored/Bidder.java
+++ AuctionCentral/refactored/Bidder.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * This class represents a Bidder that can make bids in an auction. 
+ * 
  * @author Nina
  * @author KyleD
  */
@@ -23,10 +25,25 @@ public class Bidder extends User {
 	/** The bids this bidder has made. */
 	private Map<Auction, Map<Item, BigDecimal>> myBids;
 	
+	/**
+	 * Constructs a new Bidder object.
+	 * 
+	 * @param userName is the user name of the bidder. 
+	 * @param theAddr is the address of the bidder.
+	 * @param theCredit is the credit card number of the bidder.
+	 */
 	public Bidder(String userName, String theAddr, String theCredit) {
 		this(userName, theAddr, theCredit, new HashMap<>());
 	}
 	
+	/**
+	 * Constructs a new Bidder object.
+	 * 
+	 * @param userName is the user name of the bidder.
+	 * @param theAddr is the address of the bidder.
+	 * @param theCredit is the credit card number of the bidder.
+	 * @param theBids is the bids this bidder has already made.
+	 */
 	public Bidder(String userName, String theAddr, String theCredit, 
 						Map<Auction, Map<Item, BigDecimal>> theBids) {
 		myUserName = userName;
@@ -47,7 +64,14 @@ public class Bidder extends User {
 	/**
 	 * Adds a new bid.
 	 * 
-	 * @param theBid
+	 * @param theAuction is the auction the bid is being made in.
+	 * @param theItem is the item that is being bid on.
+	 * @param theBid is the amount that is being bid.
+	 * 
+	 * @return true iff the bid was added successfully. 
+	 * 
+	 * @throws NullPointerException if theBid, theAuction or theItem are null.
+	 * @throws IllegalArgumentException if theItem is not contained in theAuction.
 	 */
 	public boolean addBid(final Auction theAuction, final Item theItem, final BigDecimal theBid) {
 		if (theBid == null || theAuction == null || theItem == null) {
@@ -69,7 +93,12 @@ public class Bidder extends User {
 	/**
 	 * Removes the specified bid.
 	 * 
-	 * @param theBid
+	 * @param theAuction is the auction where the bid was made.
+	 * @param theItem is the item that was bid on.
+	 * 
+	 * @throws NullPointerException if theAuction or theItem are null.
+	 * @throws IllegalArgumentException if a bid was not placed in this auction or
+	 *         theItem is not contained in theAuction.
 	 */
 	public void removeBid(final Auction theAuction, final Item theItem) {
 		if (theItem == null || theAuction == null) {
@@ -82,10 +111,31 @@ public class Bidder extends User {
 		theItem.removeBid(myUserName);
 	}
 	
+	/**
+	 * Checks if the bidder has made a bid on the specified item in the specified auction.
+	 * 
+	 * @param theAuction is the auction the bid was made in.
+	 * @param theItem is the item that was bid on.
+	 * 
+	 * @return true iff a bid was made in the specified auction and the auction contains the
+	 *         specified item.
+	 */
 	public boolean containsBid(final Auction theAuction, final Item theItem) {
 		return myBids.containsKey(theAuction) && myBids.get(theAuction).containsKey(theItem);
 	}
 	
+	/**
+	 * Gets the bid from the specified auction.
+	 * 
+	 * @param theAuction is the auction the bid was made in.
+	 * @param theItem is the item that was bid on.
+	 * 
+	 * @return the amount that was bid.
+	 * 
+	 * @throws NullPointerException if theAuction or theItem are null.
+	 * @throws IllegalArgumentException if a bid was not placed in this auction or
+	 *         theItem is not contained in theAuction.
+	 */
 	public BigDecimal getBidFrom(final Auction theAuction, final Item theItem) {
 		if (theItem == null || theAuction == null) {
 			throw new NullPointerException();
